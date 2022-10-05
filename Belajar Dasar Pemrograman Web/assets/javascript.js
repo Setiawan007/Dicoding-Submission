084da739da5d4b86cf92c9a5ca6659c4d4303aca
Febrianto Bagus Setiawan <febriantobagussetiawan@gmail.com>
2022-10-05 14:11:00 +0700

    Belajar Dasar Pemrograman Web

---
+++ Belajar Dasar Pemrograman Web/assets/javascript.js
@@ -0,0 +1,26 @@
+const txtElement = ['System Analyst.','Problem Solver.','Developer.'];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = '';
+let words = '';
+
+(function tik() {
+
+    if (count == txtElement.length) {
+        count = 0;
+    }
+
+    currentTxt = txtElement[count];
+
+    words = currentTxt.slice(0, ++txtIndex);
+    document.querySelector('.efek-tik').textContent = words;
+
+    if (words.length == currentTxt.length) {
+        count++;
+        txtIndex = 0;
+    }
+
+    setTimeout(tik, 500);
+
+
+})();
